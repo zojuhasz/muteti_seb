5827f73a8b4a45b69944946104eb87c2685cb969
zojuhasz <43848731+zojuhasz@users.noreply.github.com>
2026-07-16 14:11:13 +0200

    Accept DrupalDateTime in schedule service

--- web/modules/custom/muteti_seb/src/Service/Schedule.php
+++ web/modules/custom/muteti_seb/src/Service/Schedule.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\muteti_seb\Service;
 
+use Drupal\Core\Datetime\DrupalDateTime;
+
 final class Schedule {
   public const DAY_TYPES = [
     'HK' => ['B-Zu', 'B-2', 'B-3', 'B-NM', 'EP-1', 'Tu-1', 'Tu-2', 'Tu-3', 'Tu-4', 'S-1', 'S-2', 'Amb-1', 'Amb-2', 'Amb-3', 'Amb-4'],
@@ -11,7 +13,7 @@ final class Schedule {
   ];
   public const ROOMS = ['5', '6', '7', '8', 'A'];
 
-  public static function defaultDayType(\DateTimeInterface $date): string {
+  public static function defaultDayType(DrupalDateTime|\DateTimeInterface $date): string {
     return match ((int) $date->format('N')) { 1, 2 => 'HK', 3, 4 => 'SZCS', 5 => 'P', default => 'SEMMI' };
   }
 }
